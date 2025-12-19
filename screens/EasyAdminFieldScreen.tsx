13b779b247608697564e716bf83469a8509929dc
Deyios182 <thejozx.182@gmail.com>
2025-12-19 20:00:32 -0300

    Update EasyAdminFieldScreen.tsx

--- screens/EasyAdminFieldScreen.tsx
+++ screens/EasyAdminFieldScreen.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { supabase } from '../supabaseClient';
 import { Locality, Attraction, Company } from '../types';
-import { uploadImage } from '../imageHandler';
+import { uploadImage } from '../Screens/imageHandler';
 
 const EasyAdminFieldScreen: React.FC = () => {
   const [activeTab, setActiveTab] = useState<'localidades' | 'atractivos' | 'empresas'>('localidades');
